5de6baa05edc4c2c140dcd9afd6cb29fd81ea548
wk <wk>
2016-07-18 22:09:07 +0800

    商铺管理平台

--- public/js/apps/back_shop/mainpage/marketquotient/layout/layout.view.js
+++ public/js/apps/back_shop/mainpage/marketquotient/layout/layout.view.js
@@ -2,14 +2,15 @@
  * Created by wk on 2016/6/21.
  */
 define(["app", "marionette", "tpl!apps/back_shop/mainpage/marketquotient/layout/layout.html"],
-  function (BrogueApplication, Marionette, layoutTpl) {
-    BrogueApplication.module("BrogueApp.BackShop.MainPage.MarketQuotientLayout", function (View, BrogueApplication, Backbone, Marionette, $, _) {
-      View.Main = Marionette.LayoutView.extend({
-        template: layoutTpl,
-        regions: {
-          supplySupplierListRegion: ".supplySupplierListRegion"
-        }
-      });
+    function (BrogueApplication, Marionette, layoutTpl) {
+        BrogueApplication.module("BrogueApp.BackShop.MainPage.MarketQuotientLayout", function (View, BrogueApplication, Backbone, Marionette, $, _) {
+            View.Layout = Marionette.LayoutView.extend({
+                className: "container",
+                template: layoutTpl,
+                regions: {
+                    marketQuotientListRegion: ".marketQuotientListRegion"
+                }
+            });
+        });
+        return BrogueApplication.BrogueApp.BackShop.MainPage.MarketQuotientLayout;
     });
-    return BrogueApplication.BrogueApp.BackShop.MainPage.MarketQuotientLayout;
-  });
--- public/js/apps/back_shop/mainpage/marketquotient/marketquotient.controller.js
+++ public/js/apps/back_shop/mainpage/marketquotient/marketquotient.controller.js
@@ -1,19 +1,24 @@
 /**
  * Created by wk 2015年6月10日 09:20:38
  */
-define(["app", "apps/back_shop/mainpage/marketquotient/list/table.view"], function (BrogueApplication, View) {
+define(["app","apps/back_shop/mainpage/marketquotient/layout/layout.view" ,
+                "apps/back_shop/mainpage/marketquotient/list/table.view"], function (BrogueApplication,LayoutView, View) {
   BrogueApplication.module("BrogueApp.BackShop.MainPage.MarketQuotient", function (NavController, BrogueApplication, Backbone, Marionette, $, _) {
     NavController.Controller = {
       showMarketQuotientList: function (Region, xobject) {
         var that = this;
         that.xobject = xobject;
+
+        var layoutView = new LayoutView.Layout();
+        Region.show(layoutView);
+
         require(["apps/back_shop/entity/marketquotient.entities"], function () {
           var modularList = BrogueApplication.request("marketquotient:entities");
           var view = new View.Table({
             collection: modularList,
             xevent: xobject
           });
-          Region.show(view);
+          layoutView.marketQuotientListRegion.show(view);
         });
       }
     };
