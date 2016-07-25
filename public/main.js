919a069d2cc63ed23886072bc5d08eced911c5df
wk <wk>
2016-07-25 23:12:38 +0800

    商铺管理平台

--- public/main.js
+++ public/main.js
@@ -4,6 +4,7 @@ requirejs.config({
     jquery: "lib/jquery.min",
     tether: "lib/tether-1.3.1/js/tether.min",
     bootstrap: "lib/bootstrap-3.3.5/dist/js/bootstrap.min",
+    bootstrapvalidator:"lib/bootstrapvalidator/js/bootstrapValidator",
     jqueryui:"lib/jqueryui/jquery-ui",
     bootstrapmodel:"lib/bootstrap-modal",
     bootstraptransition:"lib/bootstrap-transition",
@@ -25,6 +26,7 @@ requirejs.config({
   },
   shim: {
     'bootstrap': {deps: ['jquery'], exports: 'bootstrap'},
+    'bootstrapvalidator':{deps:['jquery','bootstrap'],exports:'bootstrapvalidator'},
     'underscore': {exports: "_"},
     'backbone': {deps: ['underscore', 'jquery', "json2"], exports: 'Backbone'},
     'bootstrapmodel': {deps: ["bootstraptransition"], exports: 'bootstrapmodel'},
@@ -36,7 +38,8 @@ requirejs.config({
     'json': ["text"],
     'jqueryui':["jquery","css!lib/jqueryui/jquery-ui-1.10.3.custom.css"],
     'loginView':["css!lib/bootstrap-3.3.5/dist/css/bootstrap.min.css","css!lib/helpercss/dashboard.css","css!lib/custom-css/login.css","css!lib/custom-css/main.css"],
-    'shopMainView':["css!lib/bootstrap-3.3.5/dist/css/bootstrap.min.css","css!lib/helpercss/dashboard.css","css!lib/bootstrap-3.3.5/dist/css/font-awesome.css","css!lib/custom-css/main.css"]
+    'shopMainView':["css!lib/bootstrap-3.3.5/dist/css/bootstrap.min.css",
+      "css!lib/helpercss/dashboard.css","css!lib/bootstrap-3.3.5/dist/css/font-awesome.css","css!lib/custom-css/main.css"]
   }
 });
 
